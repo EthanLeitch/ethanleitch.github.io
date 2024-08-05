d0cbb7eaaf71d013da0d07672d078ae3dbf5524d
Ethan <ethanjohnleitch@gmail.com>
2024-08-05 21:32:24 +1200

    Changed character edit icon, add description metatag for pages

--- src/components/custom/iconbutton/edit.svelte
+++ src/components/custom/iconbutton/edit.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import Pencil from "lucide-svelte/icons/pencil";
+    import UserCog from "lucide-svelte/icons/user-cog";
     import { Button } from "$lib/components/ui/button/index.js";
   </script>
    
   <Button variant="outline" size="icon" on:click>
-    <Pencil
+    <UserCog
       class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all"
     />
     <span class="sr-only">Edit Character Attributes</span>
