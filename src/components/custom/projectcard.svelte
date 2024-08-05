abd7869370a7df86b7cc507b18def6b5c99837b1
Ethan <ethanjohnleitch@gmail.com>
2024-08-06 10:08:22 +1200

    Add GitHub links and Sitemap

--- src/components/custom/projectcard.svelte
+++ src/components/custom/projectcard.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import { Card, CardTitle, CardContent, CardFooter } from "$lib/components/ui/card";
     import { Badge } from "$lib/components/ui/badge";
+    import { Button } from "$lib/components/ui/button";
+    import EditButton from "$lib/components/custom/iconbutton/edit.svelte";
 
     let title = "";
+    let link = "";
 
     import type { HTMLAttributes } from "svelte/elements";
 	import { cn } from "$lib/utils.js";
@@ -13,7 +16,7 @@
     export { className as class };
     
     //let badges: [{"name": "", "colour": ""}] = [];
-    export { title }
+    export { title, link }
 </script>
 
 <Card class={cn("p-4", className)}>
@@ -23,9 +26,11 @@
     <CardContent>
         <slot/> 
     </CardContent>
-    <!--<CardFooter>
-        {#each badges as badge}
+    <CardFooter class="p-0">
+        <p><u><a href={"https://github.com/EthanLeitch/" + link + "?tab=readme-ov-file#" + link} target="_blank">View on GitHub</a></u></p>
+
+        <!--{#each badges as badge}
             <Badge class={badge.colour}>{badge.name}</Badge>
-        {/each}
-    </CardFooter>-->
+        {/each}-->
+    </CardFooter>
 </Card>
