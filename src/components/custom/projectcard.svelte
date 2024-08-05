c40733c9f920d1f86741fbbabb1e49873de8fded
Ethan <ethanjohnleitch@gmail.com>
2024-08-06 09:19:15 +1200

    Add homepage & navbar

---
+++ src/components/custom/projectcard.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { Card, CardTitle, CardContent, CardFooter } from "$lib/components/ui/card";
+    import { Badge } from "$lib/components/ui/badge";
+
+    let title = "";
+
+    import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils.js";
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+    let className: $$Props["class"] = undefined;
+
+    export { className as class };
+    
+    //let badges: [{"name": "", "colour": ""}] = [];
+    export { title }
+</script>
+
+<Card class={cn("p-4", className)}>
+    <CardTitle>
+        {title}
+    </CardTitle>
+    <CardContent>
+        <slot/> 
+    </CardContent>
+    <!--<CardFooter>
+        {#each badges as badge}
+            <Badge class={badge.colour}>{badge.name}</Badge>
+        {/each}
+    </CardFooter>-->
+</Card>
