20f45a5d73dfe3a41ea59b16995a016228f926f4
Ethan <ethanjohnleitch@gmail.com>
2024-08-07 11:34:20 +1200

    Added badges & mobile support to project cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind({
-    applyBaseStyles: false,
+    applyBaseStyles: true,
   })]
 });
--- src/components/custom/projectcard.svelte
+++ src/components/custom/projectcard.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
     import { Card, CardTitle, CardContent, CardFooter } from "$lib/components/ui/card";
     import { Badge } from "$lib/components/ui/badge";
-    import { Button } from "$lib/components/ui/button";
-    import EditButton from "$lib/components/custom/iconbutton/edit.svelte";
+    import ExternalLink from "lucide-svelte/icons/external-link";
 
     let title = "";
     let link = "";
@@ -14,20 +13,25 @@
     let className: $$Props["class"] = undefined;
 
     export { className as class };
-    
-    //let badges: [{"name": "", "colour": ""}] = [];
-    export { title, link }
+
+    let badges: [{"name": "", "class": ""}] = [];
+    export { title, link, badges }
 </script>
 
-<Card class={cn("p-4", className)}>
+<Card class={cn("p-4 flex flex-col", className)}>
     <CardTitle>
         {title}
     </CardTitle>
-    <CardContent>
-        <slot/> 
+    <CardContent class="p-0 py-3 px-2">
+        <slot/>
     </CardContent>
-    <CardFooter class="p-0">
-        <p><u><a href={"https://github.com/EthanLeitch/" + link + "?tab=readme-ov-file#" + link} target="_blank">View on GitHub</a></u></p>
+    <CardFooter class="p-0 px-2 flex flex-row gap-2 align-bottom mt-auto">
+        {#if badges}
+            {#each badges as badge}
+                <Badge class={badge.class}>{badge.name}</Badge>
+            {/each}
+        {/if}
+        <p><u><a href={"https://github.com/EthanLeitch/" + link + "?tab=readme-ov-file#" + link} target="_blank">GitHub ↗</a></u></p>
 
         <!--{#each badges as badge}
             <Badge class={badge.colour}>{badge.name}</Badge>
