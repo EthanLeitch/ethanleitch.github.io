79b132ef309ca51a7a092540afb97c49ec55853d
Ethan <ethanjohnleitch@gmail.com>
2024-08-06 22:45:56 +1200

    Re-add mouse cursor style (accidential deletion?)

--- src/components/custom/character/character.svelte
+++ src/components/custom/character/character.svelte
@@ -69,7 +69,7 @@
                     <Input type="text" placeholder="Name" bind:value={self.name} class="basis-1/2" /> <!-- bind:value={findInArray(id)} -->
 
                     <!-- This is fucked, but there's no other way to do it that looks good. -->
-                    <FakeInput on:click="{focusAtk}" style="flex-basis: 20.5%;" class={"flex flex-row items-center " + fakeInputClasses}> <!--relative flex flex-row items-center-->
+                    <FakeInput on:click="{focusAtk}" style="cursor: text; flex-basis: 20.5%;" class={"flex flex-row items-center " + fakeInputClasses}> <!--relative flex flex-row items-center-->
                         <p class="text-sm">d</p>
                         <input type="number" placeholder="⚔️" step=2 min=0 bind:value={self.atk} max={self.atk_max} class="flex w-full focus-visible:outline-none" bind:this={atkInput} on:focusin={() => setFakeFocus(true)} on:focusout={() => setFakeFocus(false)} /> <!-- class="text-right" -->
                     </FakeInput>
